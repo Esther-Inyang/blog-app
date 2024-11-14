223b65b89ab7e0214e1ac0474466f9d657a78ea8
DevEsti <estherinyang07@gmail.com>
2024-11-14 20:23:19 +0100

    updated loader component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,29 +1,7 @@
-import * as React from "react";
-import CircularProgress from "@mui/material/CircularProgress";
-import Box from "@mui/material/Box";
+import React from "react";
 
-interface LoaderProp {
-  color?: string
-}
+const Loader = () => {
+  return <div>Loader...</div>;
+};
 
-
-const CircularLoader: React.FC<LoaderProp> = ({ color }) => {
-  return (
-    <Box
-      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <CircularProgress sx={{ color: color || "#fff", width: "20px !important", height: "20px !important", strokeWidth: 9 }} />
-        <div className="ml-3 text-[15px]">Please wait...</div>
-      </div>
-    </Box>
-  );
-}
-
-export default  CircularLoader
+export default Loader;
